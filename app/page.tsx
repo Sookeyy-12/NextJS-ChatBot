b98e76f891d0735d0484b52b3260b07ac37a907e
Sookeyy-12 <thatonesuket@gmail.com>
2024-12-23 03:21:52 +0530

    better ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,45 @@
 'use client';
 
 import { useChat } from 'ai/react';
+import ReactMarkdown from 'react-markdown';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    maxSteps: 5,
-  });
-  return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.toolInvocations ? (
-            <pre>{JSON.stringify(m.toolInvocations, null, 2)}</pre>
-          ) : (
-            <p>{m.content}</p>
-          )}
+    const { messages, input, handleInputChange, handleSubmit } = useChat({
+        maxSteps: 5,
+    });
+    return (
+        <div className="chat flex flex-col w-full max-w-screen-sm py-24 mx-auto stretch bg-black">
+            {messages.map(m => (
+                <div key={m.id} className={`whitespace-pre-wrap m-5 ${m.role === 'user' ? 'chat-end' : 'chat-start'}`}>
+                    {m.toolInvocations ? (
+                        <>
+                            <div className='chat-header'>
+                                {m.role === 'user' ? 'User' : 'AI'}
+                            </div>
+                            <div className='chat-bubble'>
+                                <pre>{JSON.stringify(m.toolInvocations, null, 2)}</pre>
+                            </div>
+                        </>
+                    ) : (
+                    <>
+                        <div className='chat-header'>
+                            {m.role === 'user' ? 'User' : 'AI'}
+                        </div>
+                        <div className={`chat-bubble ${m.role === 'user' ? 'bg-slate-800' : 'bg-slate-900'}`}>
+                            <ReactMarkdown>{m.content}</ReactMarkdown>
+                        </div>
+                    </>
+                    )}
+                </div>
+            ))}
+            <form onSubmit={handleSubmit}>
+                <input
+                    className="fixed bottom-0 w-full max-w-screen-sm p-2 mb-8 border border-gray-300 rounded shadow-xl"
+                    value={input}
+                    placeholder="Say something..."
+                    onChange={handleInputChange}
+                />
+            </form>
         </div>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
-  );
+    );
 }
